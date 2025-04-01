2047b951735f3862947e6342897197042967dd94
Dominic Mai <dominickmai@gmail.com>
2025-03-31 21:18:29 -0700

    fixed apos in paragraph

--- frontend/app/events/summon-irein/page.tsx
+++ frontend/app/events/summon-irein/page.tsx
@@ -17,13 +17,13 @@ export default function SummonIreinPage() {
         />
       </div>
       <p>
-        What are the advantages of summoning Irein? What aren't the advantages?
-        Good Ireins are great. They will gather things for you if you whine you
-        need things, they will craft things for you if you whine you're
-        undergeared for progressing through MSQ, they'll bake a cheesecake for
-        you if you threaten to throw money at them, they'll sing musical numbers
-        if you mention you love musical theater, and they'll take fantastic
-        pictures of you with a happy trail!
+        What are the advantages of summoning Irein? What aren&apos;t the
+        advantages? Good Ireins are great. They will gather things for you if
+        you whine you need things, they will craft things for you if you whine
+        you&apos;re undergeared for progressing through MSQ, they&apos;ll bake a
+        cheesecake for you if you threaten to throw money at them, they&apos;ll
+        sing musical numbers if you mention you love musical theater, and
+        they&apos;ll take fantastic pictures of you with a happy trail!
       </p>
       <p>
         Some of these things are hypothetical. Some of them are made up. But
@@ -31,7 +31,7 @@ export default function SummonIreinPage() {
         joy!
       </p>
       <p>Theoretically.</p>
-      <p>So what do you have to do? It's simple.</p>
+      <p>So what do you have to do? It&apos;s simple.</p>
       <div className="w-full pb-[56.25%] relative">
         <Image
           src="/images/summon-irein-event-second-picture.webp"
@@ -44,16 +44,16 @@ export default function SummonIreinPage() {
         />
       </div>
       <p>
-        Don't be a shit human being and say hi to her occasionally. Really. Not
-        that hard. What? Did you expect some sort of weird summong ritual where
-        you had to sacrifice the blood of a virgin Elezen on the night of the
-        harvest moon? It doesn't work like that. Just say you need help with
-        something and she'll likely ask if she can help.
+        Don&apos;t be a shit human being and say hi to her occasionally. Really.
+        Not that hard. What? Did you expect some sort of weird summong ritual
+        where you had to sacrifice the blood of a virgin Elezen on the night of
+        the harvest moon? It doesn&apos;t work like that. Just say you need help
+        with something and she&apos;ll likely ask if she can help.
       </p>
       <p>
         Then you intermittently send sugar-free Red Bull (tm) to their doorstep
         or find some farming sim they want on their Steam wishlist and block
-        them so they can't yell at you in return.
+        them so they can&apos;t yell at you in return.
       </p>
       <p>This presentation really could have been an email.</p>
     </div>
